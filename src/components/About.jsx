abe917f8e245bb4ec8b79e171d3d9ed21c1eca93
Emmanuel Monle Nyode <emmanuel.monle@gmail.com>
2024-01-22 23:04:58 +0200

    remove background-image property from .about-image in App.css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,9 +6,12 @@ const About = () => {
       <img src={me} className="about-image" />
       <div className="about-text">
         <p>
-          &ldquo;Backend developers are animals, Frontend developers are birds
-          ... I&rsquo;m Fullstack, and I eat them both. That&rsquo;s all about
-          me. &rdquo;
+          &ldquo;Backend developers are animals. <br />
+          Frontend developers are birds. <br />
+          Prompt engineers are fish. <br />
+          WordPress designers are insects. <br />
+          Code testers are reptiles. <br />
+          ...I&rsquo;m Fullstack.&rdquo;
         </p>
       </div>
     </main>

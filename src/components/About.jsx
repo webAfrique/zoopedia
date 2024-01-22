d0ebae0a048622ce937d4e15e06201f15509b1c7
Emmanuel Monle Nyode <emmanuel.monle@gmail.com>
2024-01-22 23:37:37 +0200

    add project info to  about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,6 +13,17 @@ const About = () => {
           Code testers are reptiles. <br />
           ...I&rsquo;m Fullstack.&rdquo;
         </p>
+        <p>
+          This project is a simple wiki application that displays a collection
+          of creatures from five sub-divisions of the animal kingdom. Creatures
+          in each collection are displayed as cards in a gallery. The gallery
+          also has a searchbar to search for creatures by name within the
+          displayed collection. Each card has the name of a creature and a
+          random image obtained from unsplash.com. Cards also have a link to an
+          article about the creature on the card. The article contains an image
+          of the creature and a paragraph of biological information obtained
+          from wikipedia.com
+        </p>
       </div>
     </main>
   );
